9316deadfb960f23f1d025dbfd9a3c24105775a8
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2020-03-24 11:20:49 +0430

    adding login page

---
+++ images/templates/bookmarklet_launcher.js
@@ -0,0 +1,3 @@
+(function(){
+        document.body.appendChild(document.createElement('script')).src='https://0bcd5231.ngrok.io/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
+})();
