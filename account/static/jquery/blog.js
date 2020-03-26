d133611caaa52b1e41617b920621d774b7fbfc17
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2020-03-26 19:11:42 +0430

    build profile image of profile form

---
+++ account/static/jquery/blog.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $(".camera_div_button").on('click',function(){
+        console.log("ggg");
+        $("#ggg").click();
+    });
+
+    $("#ggg").on('change', function() {
+        $("#profile_button").click();
+    });
+//          $(".topnav").hide();
+//    $(".glyphicon").on("click", function () {
+//        var txt = $(".content").is(':visible') ? 'Read More' : 'Read Less';
+//        $(".show_hide").text(txt);
+//        $(this).next('.content').slideToggle(200);
+//        if ($(".topnav").is(":visible")) {
+//            $(".topnav").css("display", "none");
+//        }
+//    });
+});
