f1d90326246a2bcb0f2903c755fe6b05bccbef5d
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2020-03-30 00:14:59 +0430

    resign my photo template

--- account/forms.py
+++ account/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
@@ -54,3 +57,6 @@ class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('date_of_birth', 'photo')
+        widgets = {
+            'date_of_birth': DateInput(),
+        }
--- account/models.py
+++ account/models.py
@@ -7,7 +7,7 @@ User._meta.get_field('email').__dict__['_unique'] = True
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d/',blank=False,null=False,default='/static/img/person.jpg')
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/',blank=True,null=True)
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
